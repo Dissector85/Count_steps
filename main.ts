8de9e47d419a104f6cea1d902a4ca3c19c2c30ab
Dissector85 <80616305+Dissector85@users.noreply.github.com>
2021-08-09 13:26:31 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onGesture(Gesture.Shake, function () {
+    step += 1
+    basic.showString("" + (step))
+})
+let step = 0
+step = 0
+basic.showString("" + (step))
+basic.forever(function () {
+	
+})
