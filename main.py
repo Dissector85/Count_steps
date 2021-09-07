b834a7c29eabdea9df7e337bdd9125a89070d635
Dissector85 <80616305+Dissector85@users.noreply.github.com>
2021-09-07 13:41:08 +0800

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+def on_button_pressed_a():
+    basic.show_string("" + str((step)))
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global step
+    step = 0
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+step = 0
+step = 0
+
 def on_forever():
-    pass
+    global step
+    if input.acceleration(Dimension.STRENGTH) > 1500:
+        step += 1
+        music.play_tone(262, music.beat(BeatFraction.SIXTEENTH))
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,10 +1,16 @@
-input.onGesture(Gesture.Shake, function () {
-    step += 1
+input.onButtonPressed(Button.A, function () {
     basic.showString("" + (step))
+})
+input.onButtonPressed(Button.B, function () {
+    step = 0
 })
 let step = 0
 step = 0
-basic.showString("" + (step))
+radio.setGroup(1)
 basic.forever(function () {
-	
+    if (input.acceleration(Dimension.Strength) > 1500) {
+        step += 1
+        music.playTone(262, music.beat(BeatFraction.Sixteenth))
+        radio.sendString("" + (step))
+    }
 })
